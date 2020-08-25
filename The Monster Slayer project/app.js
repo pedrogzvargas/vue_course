5c805c24b3265b985f7cbc495ffb2d1865322cce
Pedro Gonzalez <pedro.gonzalez@mobicash.mx>
2020-08-25 11:46:44 -0500

    first commit

---
+++ The Monster Slayer project/app.js
@@ -0,0 +1,77 @@
+new Vue({
+    el: "#app",
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns: []
+    },
+    methods: {
+        satartGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack: function(){
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: "Player hits monster for " + damage
+            });
+            if(this.checkWin()){
+                return;
+            }
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        speciaAttack: function(){
+            this.monsterHealth -= damage = this.calculateDamage(10, 20);
+            if(this.checkWin()){
+                return;
+            }
+            this.monsterAttacks();
+        },
+        heal: function(){
+            if (this.playerHealth <= 90){
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            // this.monsterAttacks();
+        },
+        giveUp: function(){
+            this.gameIsRunning = false;
+        },
+        monsterAttacks: function(){
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: "Monster hits player for " + damage
+            });
+        },
+        calculateDamage(max, min){
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0){
+                if(confirm("You won! New Game?")){
+                    this.satartGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+                return;
+            } else if (this.playerHealth <= 0){
+                if(confirm("You lost! New Game?")){
+                    this.satartGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+                return;
+            }
+        }
+    }
+});
